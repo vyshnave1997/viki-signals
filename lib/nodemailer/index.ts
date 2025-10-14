63de9a46b7ed7f6fc1670cccfd83198e64e4b342
viki <mail.vyshnave@gmail.com>
2025-10-15 00:40:03 +0400

    implement daily news mail

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -26,20 +26,20 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
     await transporter.sendMail(mailOptions);
 }
 
-// export const sendNewsSummaryEmail = async (
-//     { email, date, newsContent }: { email: string; date: string; newsContent: string }
-// ): Promise<void> => {
-//     const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
-//         .replace('{{date}}', date)
-//         .replace('{{newsContent}}', newsContent);
+export const sendNewsSummaryEmail = async (
+    { email, date, newsContent }: { email: string; date: string; newsContent: string }
+): Promise<void> => {
+    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+        .replace('{{date}}', date)
+        .replace('{{newsContent}}', newsContent);
 
-//     const mailOptions = {
-//         from: `"Signalist News" <signalist@jsmastery.pro>`,
-//         to: email,
-//         subject: `📈 Market News Summary Today - ${date}`,
-//         text: `Today's market news summary from Signalist`,
-//         html: htmlTemplate,
-//     };
+    const mailOptions = {
+        from: `"Signalist News" <signalist@jsmastery.pro>`,
+        to: email,
+        subject: `📈 Market News Summary Today - ${date}`,
+        text: `Today's market news summary from Signalist`,
+        html: htmlTemplate,
+    };
 
-//     await transporter.sendMail(mailOptions);
-// };
+    await transporter.sendMail(mailOptions);
+};
