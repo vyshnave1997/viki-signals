49a92f9e409c91d1d0e26af9eee45b0d155fe90e
viki <mail.vyshnave@gmail.com>
2025-10-15 02:19:22 +0400

    dddd

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,9 +4,9 @@ import { useForm } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import InputField from '@/components/forms/InputField';
 import FooterLink from '@/components/forms/FooterLink';
-import {signInWithEmail, signUpWithEmail} from "@/lib/actions/auth.actions";
+import {signInWithEmail} from "@/lib/actions/auth.actions";
 import {toast} from "sonner";
-import {signInEmail} from "better-auth/api";
+
 import {useRouter} from "next/navigation";
 
 const SignIn = () => {
